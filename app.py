067aac8c2922cefe1770327214367755bc9e7d1f
LoranMadec <laurent.lolo.madec@gmail.com>
2025-01-03 16:02:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,6 +155,7 @@ def get_recommendations_globale(id_film):
 
 st.set_page_config(layout="wide")
 st.logo("https://baptistedrapeau.wordpress.com/wp-content/uploads/2019/02/logo-cave-c3a0-film-rond-blanc-noir.png", size="large", link=None, icon_image=None)
+st.header("Recherche de films et KPIs sur la Creuse et le cinéma")
 
 # Haut de la page
 st.markdown('<a id="top"></a>', unsafe_allow_html=True)  # Marqueur pour retourner en haut de la page
